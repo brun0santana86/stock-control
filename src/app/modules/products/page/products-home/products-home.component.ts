fd3dc42daa1bbb6c060a925a4a01f26a87c1ac16
Bruno de Santana <contatobrunosantana@icloud.com>
2024-01-29 15:44:44 -0300

    Criado output com event para botao adicionar produto

--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { Subject, takeUntil } from 'rxjs';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
@@ -56,7 +57,11 @@ export class ProductsHomeComponent  implements OnInit, OnDestroy{
   }
 
 
-
+handleProductAction(event: EventAction): void {
+  if (event) {
+    console.log("dados do evento recebido", event);
+  }
+}
 
   ngOnDestroy(): void {
     this.destroy$.next();
